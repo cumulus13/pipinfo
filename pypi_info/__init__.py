06ae2ee0697e25d2ce9dd6b7aa2103e9a84946c8
cumulus13 <cumulus13@gmail.com>
2026-01-03 02:22:34 +0700

    version: 0.61.9 ~ 2026-01-03 02:22:18:323026

--- pypi_info/__init__.py
+++ pypi_info/__init__.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+
+# File: pypi_info/__init__.py
+# Author: Hadi Cahyadi <cumulus13@gmail.com>
+# Date: 2026-01-03
+# Description: 
+# License: MIT
+
 __all__ = ["PyPIClient", "PackageInfoDisplay", "get_version"]
 
 try:
