fe12995ae52bfcca2e18735fb3a1a114ec18ebca
cumulus13 <cumulus13@gmail.com>
2026-01-03 02:21:19 +0700

    version: 0.61.8 ~ 2026-01-03 02:21:04:931770

--- pypi_info/__init__.py
+++ pypi_info/__init__.py
@@ -1,3 +1,11 @@
+__all__ = ["PyPIClient", "PackageInfoDisplay", "get_version"]
+
+try:
+	from .gui_qt5 import AndromedaStyle, HtmlLoaderThread, HtmlLoadWorker, LoadingOverlay, HtmlViewer, SearchLineEdit, PackageInfoWorker, PyPIInfoGUI,  # type: ignore 
+	__all__.extend(["AndromedaStyle", "HtmlLoaderThread", "HtmlLoadWorker", "LoadingOverlay", "HtmlViewer", "SearchLineEdit", "PackageInfoWorker", "PyPIInfoGUI"])
+except:
+	pass
+
 from .pipinfo import PyPIClient, PackageInfoDisplay, get_version
 
-__all__ = ["PyPIClient", "PackageInfoDisplay", "get_version"]
+
