6afaeeee009217bf181ce12031eef5a1f2665cca
cumulus13 <cumulus13@gmail.com>
2026-01-03 02:25:06 +0700

    version: 0.61.10 ~ 2026-01-03 02:24:51:641698

--- pypi_info/__init__.py
+++ pypi_info/__init__.py
@@ -9,7 +9,7 @@
 __all__ = ["PyPIClient", "PackageInfoDisplay", "get_version"]
 
 try:
-	from .gui_qt5 import AndromedaStyle, HtmlLoaderThread, HtmlLoadWorker, LoadingOverlay, HtmlViewer, SearchLineEdit, PackageInfoWorker, PyPIInfoGUI,  # type: ignore 
+	from .gui_qt5 import AndromedaStyle, HtmlLoaderThread, HtmlLoadWorker, LoadingOverlay, HtmlViewer, SearchLineEdit, PackageInfoWorker, PyPIInfoGUI  # type: ignore 
 	__all__.extend(["AndromedaStyle", "HtmlLoaderThread", "HtmlLoadWorker", "LoadingOverlay", "HtmlViewer", "SearchLineEdit", "PackageInfoWorker", "PyPIInfoGUI"])
 except:
 	pass
